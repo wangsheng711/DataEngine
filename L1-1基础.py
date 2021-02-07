c759e9e344342be81370a8a7989d61b1f4a66a4e
wangsheng711 <60723728+wangsheng711@users.noreply.github.com>
2021-02-07 09:54:49 +0800

    Update L1-1基础.py

--- L1-1基础.py
+++ L1-1基础.py
@@ -1,4 +1,12 @@
-# 求2+4+6+8+...+100的求和
+# 求2+4+6+8+...+100的求和，方法1
+sum = 0
+number = 2
+while number < 101:
+    sum = sum + number
+    number = number + 2
+print(sum)
+
+# 求2+4+6+8+...+100的求和，方法2
 import numpy as np
 sum = 0
 for x in np.arange(2,101,2):
